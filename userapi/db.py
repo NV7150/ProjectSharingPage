d296eb32d4f89374b7660b7d1f6d8f9f799e7e75
Hiroki.T <hirodora@me.com>
2020-12-15 03:44:38 +0900

    add: connect to postgres

---
+++ userapi/db.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative.api import DeclarativeMeta
+
+import os
+from contextlib import contextmanager
+
+
+PG_USER = os.environ.get('POSTGRES_USER')
+PG_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+DB_URL = f'postgresql://{PG_USER}:{PG_PASSWORD}@db'
+DB_ECHO = (os.environ.get('DB_ECHO') != 'False')
+
+engine = create_engine(
+    DB_URL,
+    echo=DB_ECHO
+)
+
+Session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=True, bind=engine)
+)
+
+
+@contextmanager
+def session_scope() -> scoped_session:
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        raise e
+    finally:
+        session.close()
+
+
+Base: DeclarativeMeta = declarative_base()
--- userapi/main.py
+++ userapi/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+import db
+
 
 app = FastAPI(
     docs_url="/userapi/docs",
     openapi_url="/userapi/openapi.json",
 )
+
+
+# Init db
+db.Base.metadata.create_all(bind=db.engine)
+
 
 @app.get("/userapi/")
 async def index():
