b000fb040618b3fcdb08a3d999f17c83f29199c3
NV7150 <7150macNV777DAI@gmail.com>
2020-12-17 15:16:10 +0900

    create: ProjectCard

---
+++ front/project-sharing-page/src/views/Project.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <p>This user Id is {{$route.params.projectId}}</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Project"
+}
+</script>
+
+<style scoped>
+
+</style>
