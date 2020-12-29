d012a2cf62e5b7199225d4c0f4fbca184975c0d4
Hiroki.T <hirodora@me.com>
2020-12-30 02:41:53 +0900

    create: Project database

---
+++ projectapi/db.py
@@ -0,0 +1,117 @@
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative.api import DeclarativeMeta
+from sqlalchemy import Column, Integer, String
+
+import os
+from contextlib import contextmanager
+from typing import List
+
+
+PG_USER = os.environ.get('POSTGRES_USER')
+PG_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+DB_URL = f'postgresql://{PG_USER}:{PG_PASSWORD}@projectdb'
+DB_ECHO = (os.environ.get('DB_ECHO') != 'False')
+
+engine = create_engine(
+    DB_URL,
+    echo=DB_ECHO,
+)
+
+Session = scoped_session(
+    sessionmaker(
+        autocommit=False, autoflush=True,
+        bind=engine, expire_on_commit=False,
+    )
+)
+
+
+@contextmanager
+def session_scope() -> scoped_session:
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        raise e
+    finally:
+        session.close()
+
+
+Base: DeclarativeMeta = declarative_base()
+
+
+class Project(Base):
+    """[DB] Project Class
+
+    Parameters
+    ----------
+    id: int (auto)
+    title: str
+    subtitle: Optional[str]
+    bg_image: Optional[str]
+    description: str
+    members: List[str]
+        list of username
+    tags: List[int]
+
+    twitter: Optional[str]
+    instagram: Optional[str]
+    github: Optional[str]
+    youtube: Optional[str]
+    vimeo: Optional[str]
+    facebook: Optional[str]
+    tiktok: Optional[str]
+    linkedin: Optional[str]
+    wantedly: Optional[str]
+    url: Optional[str]
+    """
+    __tablename__ = 'project'
+    id = Column('id', Integer, primary_key=True)
+    title = Column('title', String, nullable=False)
+    subtitle = Column('subtitle', String, nullable=True)
+    bg_image = Column('bg_image', String, nullable=True)
+    description = Column('description', String, nullable=False)
+    __members = Column('members', String, nullable=False)
+
+    # SNS
+    twitter = Column('twitter', String, nullable=True)
+    instagram = Column('instagram', String, nullable=True)
+    github = Column('github', String, nullable=True)
+    youtube = Column('youtube', String, nullable=True)
+    vimeo = Column('vimeo', String, nullable=True)
+    facebook = Column('facebook', String, nullable=True)
+    tiktok = Column('tiktok', String, nullable=True)
+    linkedin = Column('linkedin', String, nullable=True)
+    wantedly = Column('wantedly', String, nullable=True)
+    url = Column('url', String, nullable=True)
+
+    # skill
+    __skilltags = Column('skilltags', String, nullable=False)
+
+    @property
+    def skilltags(self) -> List[int]:
+        return [
+            int(tag.replace(' ', ''))
+            for tag in self.__skilltags.split(',')
+            if tag not in ['', ' ']
+        ]
+
+    @skilltags.setter
+    def skilltags(self, tagid_list: List[int]):
+        s = ','.join([str(t) for t in tagid_list])
+        self.__skilltags = s
+
+    @property
+    def members(self) -> List[int]:
+        return [
+            int(member.replace(' ', ''))
+            for member in self.__members.split(',')
+            if member not in ['', ' ']
+        ]
+
+    @members.setter
+    def members(self, members: List[int]):
+        self.__members = ','.join([str(m) for m in members])
