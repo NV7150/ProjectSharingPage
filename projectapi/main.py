1a0536c66681295daeb7d76f9e1ad08628dd35df
Hiroki.T <hirodora@me.com>
2021-01-03 00:48:02 +0900

    add: PATCH /projectapi/project/{id:int}/like

--- projectapi/main.py
+++ projectapi/main.py
@@ -1,7 +1,8 @@
 from typing import Optional
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Cookie
 import db
 import schema
+from utils import auth
 
 
 # Init db
@@ -18,6 +19,8 @@ app = FastAPI(
 async def index():
     return {'message': 'Hello, projectapi!'}
 
+
+# CRUD Project
 
 @app.get(
     '/projectapi/project/{id:int}',
@@ -99,3 +102,51 @@ async def delete_project(id: int):
 
         p.is_active = False
         s.commit()
+
+
+# Like
+
+@app.patch(
+    '/projectapi/project/{id:int}/like',
+    description='like to project',
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            'description': 'Successful response',
+        },
+        status.HTTP_429_TOO_MANY_REQUESTS: {
+            'description': 'Already liked',
+        },
+        status.HTTP_401_UNAUTHORIZED: {
+            'description': 'Cookie token is required.',
+        },
+        status.HTTP_404_NOT_FOUND: {
+            'description': 'Project not found.',
+        },
+    }
+)
+async def like(id: int, token: Optional[str] = Cookie(None)):
+    if token is None:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    username: Optional[str] = auth.auth(token)
+    if username is None:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+
+    with db.session_scope() as s:
+        p: Optional[db.Project] = s.query(db.Project).get(id)
+        if p is None:
+            raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+        user_proj_likes = s.query(db.Like).filter(
+            db.Like.username == username
+        ).filter(
+            db.Like.project_id == id
+        )
+        if user_proj_likes.count() > 0:
+            raise HTTPException(status.HTTP_429_TOO_MANY_REQUESTS)
+
+        like = db.Like(username=username, project_id=p.id)
+        s.add(like)
+        s.commit()
+
+        return
