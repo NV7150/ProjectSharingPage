6e184c75c8da32ed57686ac0ed77507fa66163a0
Hiroki.T <hirodora@me.com>
2021-01-03 15:15:36 +0900

    fix: some bugs

--- projectapi/main.py
+++ projectapi/main.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 from fastapi import FastAPI, HTTPException, status, Cookie
-from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND
 import db
 import schema
 from utils import auth
@@ -95,7 +95,7 @@ async def update_project(
     project_update: schema.ProjectUpdate,
     token: Optional[str] = Cookie(None),
 ):
-    # Permission
+    # Permission (admin only)
     if token is None:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
@@ -103,10 +103,10 @@ async def update_project(
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
     with db.session_scope() as s:
-        t = s.query(db.Project).get(project_update.id)
-        if t is None:
+        p = db.Project.get(s, project_update.id)
+        if p is None:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
-        if username not in [au.username for au in t.admin_users]:
+        if username not in [au.username for au in p.admin_users]:
             raise HTTPException(status.HTTP_403_FORBIDDEN)
 
     # Update
@@ -127,13 +127,22 @@ async def update_project(
         status.HTTP_404_NOT_FOUND: {
             'description': 'Project not found',
         },
+        status.HTTP_401_UNAUTHORIZED: {
+            'description': 'login failed'
+        },
+        status.HTTP_403_FORBIDDEN: {
+            'description': 'forbidden (admin only)'
+        },
     },
 )
-async def delete_project(id: int):
+async def delete_project(id: int, token: Optional[str] = Cookie(None)):
     with db.session_scope() as s:
-        p: Optional[db.Project] = s.query(db.Project).get(id)
+        p = db.Project.get(s, id)
         if p is None:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+        # p.
+        # TODO: token auth, permission
 
         p.is_active = False
         s.commit()
@@ -157,7 +166,7 @@ async def delete_project(id: int):
 )
 def get_likes(id: int):
     with db.session_scope() as s:
-        p = s.query(db.Project).get(id)
+        p = db.Project.get(s, id)
         if p is None:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
 
@@ -191,7 +200,7 @@ async def like(id: int, token: Optional[str] = Cookie(None)):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
     with db.session_scope() as s:
-        p: Optional[db.Project] = s.query(db.Project).get(id)
+        p = db.Project.get(s, id)
         if p is None:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
 
@@ -275,4 +284,5 @@ async def projects_of_user(username: str):
         return [
             schema.Project.from_db(pu.project)
             for pu in proj_list
+            if pu.project.is_active
         ]
