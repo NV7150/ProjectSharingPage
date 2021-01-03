d3db513ff9ac91a13d337b6dc7bfcac1532e9846
NV7150 <7150macNV777DAI@gmail.com>
2021-01-03 18:02:10 +0900

    update: ユーザページのデザインを改訂

--- front/project-sharing-page/src/components/User/UserProjects/UserProjectCard.vue
+++ front/project-sharing-page/src/components/User/UserProjects/UserProjectCard.vue
@@ -4,20 +4,13 @@
       @mouseover="hovered = true"
       @mouseleave="hovered = false"
       :height="height"
+      width="100%"
   >
-    <template slot="progress">
-      <v-progress-linear
-          color="deep-purple"
-          height="10"
-          indeterminate
-      >
-      </v-progress-linear>
-    </template>
 
     <v-img
         height="100%"
         :src="project.bg_image"
-        class="align-end"
+        class="align-center"
     >
       <v-card-title class="white--text"> {{project.title}} </v-card-title>
     </v-img>
@@ -27,17 +20,18 @@
           v-if="hovered"
           class="transition-fast-in-fast-out hover-trans"
       >
-        <v-card-title v-text="project.title"></v-card-title>
         <v-card-text>
-          <p>{{project.subtitle}}</p>
-          <v-btn
-            text
-            :disabled="loading || failed"
-            color="real accent-4"
-            @click="toProjectPage"
-          >
-            See More
-          </v-btn>
+          <v-row align="center" class="pl-3">
+            {{project.subtitle}}
+            <v-spacer></v-spacer>
+            <v-btn
+              icon
+              :disabled="loading || failed"
+              @click="toProjectPage"
+            >
+              <v-icon>mdi-page-next</v-icon>
+            </v-btn>
+          </v-row>
         </v-card-text>
       </v-card>
     </v-expand-transition>
@@ -49,7 +43,7 @@ import axios from "axios";
 import ProjectPageSettings from "@/assets/scripts/ProjectPageSettings";
 
 export default {
-  name: "ProjectCard",
+  name: "UserProjectCard",
   props: {
     projectId : {
       type: Number,
@@ -70,36 +64,46 @@ export default {
       failed : false
     }
   },
-  created() {
-    axios
-        .get("projectapi/project/" + this.projectId)
-        .then((response) => {
-          this.project = response.data;
-          this.loading = false;
-        })
-        .catch(() => {
-          this.project = ProjectPageSettings.failedProject;
-          this.loading = false;
-          this.failed = true;
-        });
-  },
   methods:{
     toProjectPage() {
       this.$router.push({
         name:'Project',
         params: { projectId: this.project.id }
       })
+    },
+    getProject(){
+      console.log(this.projectId);
+      axios
+          .get("/projectapi/project/" + this.projectId)
+          .then((response) => {
+            this.project = response.data;
+            this.loading = false;
+          })
+          .catch(() => {
+            this.project = ProjectPageSettings.failedProject;
+            this.loading = false;
+            this.failed = true;
+          });
     }
-  }
+  },
+
+  created() {
+    this.getProject();
+  },
+  // watch: {
+  //   projectId: function (){
+  //     this.getProject();
+  //   }
+  // }
 }
 </script>
 
 <style scoped>
-  .hover-trans{
-    height: 100%;
-    bottom: 0;
-    opacity: 0.8 !important;
-    position: absolute;
-    width: 100%;
-  }
+.hover-trans{
+  height: 100%;
+  bottom: 0;
+  opacity: 0.8 !important;
+  position: absolute;
+  width: 100%;
+}
 </style>
--- front/project-sharing-page/src/views/User.vue
+++ front/project-sharing-page/src/views/User.vue
@@ -1,49 +1,30 @@
 <template>
   <v-main>
     <Navigation></Navigation>
-    <v-container class="d-flex">
-      <v-row align="center" justify="center">
-        <v-item-group
-          v-model="window"
-          class="shrink mr-6 flex-row"
-          mandatory
-          tag="v-flex"
-        >
-          <v-item
-            v-for="i in 3"
-            :key="i"
-            v-slot:default="{active, toggle}"
-          >
-            <div>
-              <v-btn
-                :input-value="active"
-                icon
-                @click="toggle"
-              >
-                <v-icon>mdi-record</v-icon>
-              </v-btn>
-            </div>
-          </v-item>
-        </v-item-group>
 
-        <v-col
-        >
-          <v-window
-            v-model="window"
-            vertical
-          >
-            <v-window-item>
-              <UserProfile :user="user"></UserProfile>
-            </v-window-item>
+    <v-container>
+      <v-row>
+        <v-col md="10" cols="12">
+          <UserProfile :user="user" />
+        </v-col>
+<!--        Show LARGER than md -->
+        <v-col md="2" class="d-md-flex d-none">
+          <UserContacts :user="user" />
+        </v-col>
+      </v-row>
 
-            <v-window-item>
-              <UserProjects :user="user"></UserProjects>
-            </v-window-item>
+<!--      Show SMALLER than md -->
+      <v-row class="d-md-none">
+        <v-col cols="12">
+          <UserContacts :user="user" />
+        </v-col>
+      </v-row>
+    </v-container>
 
-            <v-window-item>
-              <UserContacts :user="user"></UserContacts>
-            </v-window-item>
-          </v-window>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <UserProjects :user="user" />
         </v-col>
       </v-row>
     </v-container>
@@ -56,7 +37,6 @@ import Navigation from "../components/Navigation/Navigation";
 import UserProfile from "../components/User/UserProfile";
 import UserProjects from "../components/User/UserProjects";
 import UserContacts from "../components/User/UserContacts";
-// import PlaceHolder from "../assets/PlaceHolder.png";
 
 export default {
   name: "User",
