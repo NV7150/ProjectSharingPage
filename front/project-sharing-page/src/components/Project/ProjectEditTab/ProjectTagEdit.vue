c1156ea07bc64d39b9c3315cb31abfbf9c42e0cc
NV7150 <7150macNV777DAI@gmail.com>
2021-02-04 21:03:16 +0900

    update: プロジェクトのタグ編集をとりあえず実装

--- front/project-sharing-page/src/components/Project/ProjectEditTab/ProjectTagEdit.vue
+++ front/project-sharing-page/src/components/Project/ProjectEditTab/ProjectTagEdit.vue
@@ -6,7 +6,7 @@
 
 <script>
 import TagRegister from "@/components/Bases/Edit/TagRegister";
-// import axios from "axios";
+import axios from "axios";
 
 export default {
   name: "ProjectTagEdit",
@@ -23,24 +23,25 @@ export default {
 
   methods: {
     editProject(tagId){
-      console.log(tagId);
-      // if(this.nowTags.indexOf(tagId) !== -1)
-      //   return;
-      // this.nowTags.push(tagId);
-      // let newProject = {
-      //   "skilltags": this.nowTags
-      // };
+      if(this.nowTags.indexOf(tagId) !== -1)
+        return;
+      this.nowTags.push(tagId);
+      this.nowTags = this.nowTags.filter(v => (typeof v) === "number");
 
-      // this.isLoading = true;
-      // axios
-      //     .patch("/projectapi/project?json_data=" + JSON.stringify(newProject))
-      //     .then(() => {
-      //       this.isLoading = false;
-      //     })
-      //     .catch(() => {
-      //       //TODO:エラー処理
-      //       alert("ERROR in patch Project");
-      //     });
+      let newProject = {
+        "skilltags": this.nowTags
+      };
+
+      this.isLoading = true;
+      axios
+          .patch("/projectapi/project/" + this.project.id + "?update_fields=" + JSON.stringify(newProject))
+          .then(() => {
+            this.isLoading = false;
+          })
+          .catch(() => {
+            //TODO:エラー処理
+            alert("ERROR in patch Project");
+          });
     }
   },
 
@@ -48,7 +49,7 @@ export default {
     this.nowTags = []
     let tags = this.project.skilltags;
     for(let i = 0; i < tags.length; i++){
-      this.nowTags.push([tags[i]].id);
+      this.nowTags.push(tags[i]);
     }
   }
 }
