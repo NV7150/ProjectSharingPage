867e01b98ef3bc26698070ef8eeaa8cdd2f9c37a
Hiroki.T <hirodora@me.com>
2021-02-07 18:05:04 +0900

    add: get all skilltag

--- recommendapi/main.py
+++ recommendapi/main.py
@@ -17,8 +17,9 @@ async def index():
 
 class Recommend(object):
     def __init__(self, token: str) -> None:
+        self.token = token
         self.__get_user()  # get user
-        self.__get_userskilltags()  # get skilltags
+        self.raw_user_skilltags: List[Dict] = self.user['skilltags']
 
         # 持っているタグ
         self.tags: List[int] = [t['id'] for t in self.raw_user_skilltags]
@@ -26,13 +27,11 @@ class Recommend(object):
         # 親タグ (父母まで。祖父母までは今の所遡らない)
         self.parent_tags: List[int] = []
         for t in self.raw_user_skilltags:
-            self.parent_tags.append(
-                *[t.id for t in t['parents']]
-            )
+            for p in t['parents']:
+                self.parent_tags.append(p['id'])
 
-        # 子タグ
-
-        # 兄弟タグ(親タグが共通)
+        self.__get_children()  # 子タグ
+        self.__get_bros()  # 兄弟タグ(親タグが共通)
 
     def __get_user(self):
         cookies = {'token': self.token}
@@ -48,20 +47,38 @@ class Recommend(object):
         user = userresp.json()
         self.user = user
 
-    def __get_userskilltags(self):
+    def __get_children(self):
         cookies = {'token': self.token}
-        userresp = requests.get('http://userapi:8000/userapi/user',
-                                cookies=cookies)
-        if userresp.status_code != 200:
-            if userresp.status_code == 404:
-                raise HTTPException(status.HTTP_404_NOT_FOUND,
-                                    'User not found.')
-            else:
+
+        self.child_tags: List[int] = []
+        for t in self.tags:
+            resp = requests.get(
+                f'http://userapi:8000/userapi/skilltag/{t}/children',
+                cookies=cookies
+            )
+            if resp.status_code != 200:
                 raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                    'Can\'t get user info')
-        user = userresp.json()
-        skilltags: List[Dict] = user['skilltags']
-        self.raw_user_skilltags = skilltags
+                                    f'Can\'t get child tags (tag={t})')
+            children: List[Dict] = resp.json()
+            for c in children:
+                self.child_tags.append(c['id'])
+
+    def __get_bros(self):
+        cookies = {'token': self.token}
+
+        self.bro_tags: List[int] = []
+        for t in self.tags:
+            resp = requests.get(
+                f'http://userapi:8000/userapi/skilltags/{t}/bros',
+                cookies=cookies
+            )
+
+            if resp.status_code != 200:
+                raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                    f'Can\'t get bros (tag={t})')
+            bros: List[Dict] = resp.json()
+            for b in bros:
+                self.bro_tags.append(b['id'])
 
 
 @app.get(
@@ -89,3 +106,4 @@ async def project(token: Optional[str] = Cookie(None)):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
     r = Recommend(token)
+    return r
