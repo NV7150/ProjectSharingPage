1c969f92c6a6ac8324e4811fda0a4521d145f5ac
Hiroki.T <hirodora@me.com>
2021-02-07 21:11:36 +0900

    add: gather projects

--- recommendapi/main.py
+++ recommendapi/main.py
@@ -16,6 +16,9 @@ async def index():
 
 
 class Recommend(object):
+    # TODO: 1. 点数付け. ソート.
+    # TODO: 2. ランダムなプロジェクトを取ってくる. ランダムに挿入
+
     def __init__(self, token: str) -> None:
         self.token = token
         self.__get_user()  # get user
@@ -30,12 +33,32 @@ class Recommend(object):
             for p in t['parents']:
                 self.parent_tags.append(p['id'])
 
+        self.child_tags: List[int] = []
+        self.bro_tags: List[int] = []
         self.__get_children()  # 子タグ
         self.__get_bros()  # 兄弟タグ(親タグが共通)
 
-        # TODO: 1. それぞれのタグが該当するプロジェクトを集めてくる
-        # TODO: 2. 点数付け. ソート.
-        # TODO: 3. ランダムなプロジェクトを取ってくる. ランダムに挿入
+        self.tag_projects: List[Dict] = []
+        self.parent_tag_projects: List[Dict] = []
+        self.child_tag_projects: List[Dict] = []
+        self.bro_tag_projects: List[Dict] = []
+
+    def gather_projects(self):
+        pairs = [
+            (self.tags, self.tag_projects),
+            (self.parent_tags, self.parent_tag_projects),
+            (self.child_tags, self.child_tag_projects),
+            (self.bro_tags, self.bro_tag_projects),
+        ]
+        for tags, projects in pairs:
+            query = {'tags': tags}
+            resp = requests.get('http://projectapi:8000/projectapi/project',
+                                params=query)
+            if resp.status_code != 200:
+                raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
+            projects.clear()
+            for p in resp.json():
+                projects.append(p)
 
     def __get_user(self):
         cookies = {'token': self.token}
@@ -53,8 +76,6 @@ class Recommend(object):
 
     def __get_children(self):
         cookies = {'token': self.token}
-
-        self.child_tags: List[int] = []
         for t in self.tags:
             resp = requests.get(
                 f'http://userapi:8000/userapi/skilltag/{t}/children',
@@ -69,8 +90,6 @@ class Recommend(object):
 
     def __get_bros(self):
         cookies = {'token': self.token}
-
-        self.bro_tags: List[int] = []
         for t in self.tags:
             resp = requests.get(
                 f'http://userapi:8000/userapi/skilltags/{t}/bros',
@@ -110,4 +129,5 @@ async def project(token: Optional[str] = Cookie(None)):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
     r = Recommend(token)
-    return r
+    r.gather_projects()
+    return r  # DEBUG
