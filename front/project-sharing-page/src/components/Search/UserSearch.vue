c9fd70873fac967b659b9e23572f29a9d521b126
NV7150 <7150macNV777DAI@gmail.com>
2021-02-15 23:11:39 +0900

    update: ユーザ検索を実装

--- front/project-sharing-page/src/components/Search/UserSearch.vue
+++ front/project-sharing-page/src/components/Search/UserSearch.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card :loading="isLoading">
+  <v-card :loading="isLoading" :disabled="isLoading">
 
     <template slot="progress">
       <v-progress-linear
@@ -9,32 +9,37 @@
       />
     </template>
 
-    <v-toolbar>
-      <v-toolbar-title>
-        Users
-      </v-toolbar-title>
-    </v-toolbar>
+    <template v-if="!isLoading">
+      <v-container v-if="results.length > 0">
+        <v-row>
+          <v-col
+              cols="12"
+              sm="6"
+              md="4"
+              v-for="(resultUser, i) in results"
+              :key="i"
+          >
+            <UserCard :username="resultUser.username" />
+          </v-col>
+        </v-row>
+      </v-container>
 
-    <v-container v-if="!isLoading">
-      <v-row>
-        <v-col
-            cols="12"
-            sm="6"
-            md="4"
-            lg="3"
-            xl="2"
-            v-for="(resultUser, i) in results"
-            :key="i"
-        >
-          <UserCard :username="resultUser.username" />
-        </v-col>
-      </v-row>
-    </v-container>
+      <v-container v-else>
+        <v-row justify="center" align="center" >
+          <v-col class="text-center font-weight-light">
+            No users found
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
   </v-card>
 </template>
 
 <script>
 import UserCard from "@/components/Search/UserSearch/UserCard";
+import axios from "axios";
+
+import ErrorResolver from "@/assets/scripts/ErrorResolver";
 
 export default {
   name: "UserSearch",
@@ -44,10 +49,26 @@ export default {
   },
   data(){
     return{
-      //TODO:仮置き削除
-      results: [{username: "tester1"}],
-      isLoading: false
+      results: [],
+      isLoading: false,
     };
+  },
+  created() {
+    this.isLoading = true;
+    axios
+        .get("/userapi/user/search", {
+          params: {
+            keyword: this.keyword
+          }
+        })
+        .then((response) => {
+          this.results = response.data.all_result;
+          this.isLoading = false;
+        })
+        .catch(() => {
+          ErrorResolver.resolveError(this.$router);
+        });
+
   }
 }
 
