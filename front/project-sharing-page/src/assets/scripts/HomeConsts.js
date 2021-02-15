d412518fa675005b05b98fa14f286563aeefeedb
NV7150 <7150macNV777DAI@gmail.com>
2021-02-15 17:27:33 +0900

    update: Homeを非ログインユーザにも対応，ホーム画面にページを追加

---
+++ front/project-sharing-page/src/assets/scripts/HomeConsts.js
@@ -0,0 +1,3 @@
+export default {
+    projectPerPage : 6
+}
--- front/project-sharing-page/src/components/Home/Projects.vue
+++ front/project-sharing-page/src/components/Home/Projects.vue
@@ -1,24 +1,25 @@
 <template>
   <v-container>
-    <v-row v-if="!isLoading">
-      <v-col
-          cols="12"
-          sm="6"
-          md="4"
-          lg="3"
-          xl="2"
-          v-for="(recommend, i) in recommends"
-          :key="i"
-      >
-        <v-lazy
-            v-model="isActive[i]"
-            :options="{threshold: .5}"
-            transition="fade-transition"
+    <v-container v-if="!isLoading">
+      <v-row>
+        <v-col
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+            xl="2"
+            v-for="(recommend, i) in recommends"
+            :key="i"
         >
           <ProjectCard :project-id="recommend"></ProjectCard>
-        </v-lazy>
-      </v-col>
-    </v-row>
+        </v-col>
+      </v-row>
+
+      <div class="text-center">
+        <v-pagination v-model="page" :length="length" />
+      </div>
+    </v-container>
+
     <v-container v-else>
       <v-row align="center" justify="center" >
         <v-progress-circular
@@ -39,40 +40,115 @@
 </template>
 
 <script>
-import ProjectCard from "./ProjectCard";
 import axios from "axios";
+
+import ProjectCard from "./ProjectCard";
+
 import ErrorResolver from "@/assets/scripts/ErrorResolver";
+import HomeConsts from "@/assets/scripts/HomeConsts";
 
 export default {
   name: "Projects",
   components: {ProjectCard},
   data(){
     return{
+      length: 0,
       recommends: [],
       isLoading : false,
-      isActive: []
+      page: 1
     }
   },
-  mounted() {
-    if(!this.$store.getters["getUser"]){
-      //TODO:ログイン者じゃない向けの処理
-      this.recommends=[1,2,3,4];
-      return;
+  methods: {
+    getCount(){
+      return new Promise((resolve, reject) => {
+        axios
+            .get("/projectapi/project/count")
+            .then((response) => {
+              this.length = Math.floor((Number)(response.data) / HomeConsts.projectPerPage) + 1;
+              this.page = 1;
+              resolve();
+            })
+            .catch(() => {
+              reject();
+            });
+      })
+    },
+
+    getLoginRecommend(){
+      return new Promise((resolve, reject) => {
+        axios
+            .get("/recommendapi/project", {
+              params: {
+                limit: HomeConsts.projectPerPage,
+                offset: (this.page - 1) * HomeConsts.projectPerPage
+              }
+            })
+            .then((response) => {
+              this.responseRecommend(response.data);
+              resolve();
+            })
+            .catch(() => {
+              reject();
+            });
+      });
+    },
+    getNotLoginRecommend(){
+      return new Promise((resolve, reject) => {
+        axios
+            .get("/recommendapi/project/no-user", {
+              params: {
+                limit: HomeConsts.projectPerPage,
+                offset: (this.page - 1) * HomeConsts.projectPerPage
+              }
+            })
+            .then((response) => {
+              this.responseRecommend(response.data);
+              resolve();
+            })
+            .catch(() => {
+              reject();
+            });
+      });
+    },
+    responseRecommend(data){
+      this.recommends = data;
+    },
+
+
+    getRecommend(){
+      let user = this.$store.getters["getUser"];
+      if(!user || !user["skilltags"].length || user["skilltags"].length <= 0){
+        return this.getNotLoginRecommend();
+      }
+      return this.getLoginRecommend();
     }
+  },
+
+  created() {
 
     this.isLoading = true;
-    axios
-        .get("/recommendapi/project")
-        .then((response) => {
-          this.recommends = response.data;
+    Promise.resolve()
+        .then(this.getCount)
+        .then(this.getRecommend)
+        .then(() => {
           this.isLoading = false;
-          for(let i = 0; i < this.recommends.length; i++){
-            this.isActive.push(false);
-          }
         })
         .catch(() => {
           ErrorResolver.resolveError(this.$router);
         });
+  },
+
+  watch: {
+    page: function (){
+      this.isLoading = true;
+      this.getRecommend()
+          .then(() => {
+            this.isLoading = false;
+          })
+          .catch(() => {
+            ErrorResolver.resolveError(this.$router);
+          });
+    }
   }
 }
 </script>
